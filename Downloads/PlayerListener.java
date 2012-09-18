58935a0423b998bc4e7d1a2918b25b1ed7b9a846
ludo0777 <peter.wathall@gmail.com>
2012-09-18 19:20:22 +0200

    Added PlayerJoinEvent
    Basically...

--- Downloads/PlayerListener.java
+++ Downloads/PlayerListener.java
@@ -1,4 +1,30 @@
 import org.bukkit.event.Listener;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.ChatColor;
  
 public class PlayerListener implements Listener {
+      
+      @EventHandler(priority = EventPriority.HIGHEST)
+    public void PlayerJoin(final PlayerJoinEvent event) {
+        Player p = event.getPlayer();
+     if (p.getName().equals("MYCRAFTisbest")) {
+           Bukkit.broadcastMessage(ChatColor.BLUE + "[MyIsle]" + ChatColor.GOLD + "MYCRAFTisbest made MyIsle");
+    	}
+    	      else if (p.getName().equals("evilmidget38")) {
+    	    	  Bukkit.broadcastMessage(ChatColor.BLUE + "[MyIsle] " + ChatColor.GOLD + "evilmidget38 made MyIsle");
+    	      }
+    	      else if (p.getName().equals("Husky")){
+    	    	  Bukkit.broadcastMessage(ChatColor.BLUE + "[MyIsle] " + ChatColor.GOLD + "Husky made MyIsle");
+    	      } else if (p.getName().equals("ludo0777")) {
+    	    	  Bukkit.broadcastMessage(ChatColor.BLUE + "[MyIsle] " + ChatColor.GOLD + "ludo0777 made MyIsle");
+    	      }
+    	      else{
+    	    	  Bukkit.broadcastMessage(ChatColor.BLUE + "[MyIsle]" + ChatColor.GREEN + p.getName() + "has joined as level <var>");
+    	    	 ((Player) p).sendMessage(ChatColor.BLUE + "[MyIsle]" + ChatColor.GREEN + "This is a Isle server");
+    	      }
+    	if (voteMsg) return;{
+    		((Player) p).sendMessage(ChatColor.BLUE + "[MyIsle]" + ChatColor.GREEN + "Vote for the server and get a larger isle");
+    	}
+	}
     }
