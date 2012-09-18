a8b8df71c1de2ab1955ccd6fb539952512083c4c
ludo0777 <peter.wathall@gmail.com>
2012-09-18 21:01:14 +0200

    Added creaturespawnevent

--- Downloads/OtherListener.java
+++ Downloads/OtherListener.java
@@ -1,4 +1,8 @@
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
  
 public class OtherListener implements Listener {
+ 
+      @EventHandler(priority = EventPriority.HIGHEST)
+        public void onCreatureSpawn(CreatureSpawnEvent event) {
     }
