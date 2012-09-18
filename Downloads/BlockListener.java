f8b85f4af8a63422dc6e368e67ecad88e1976d8f
ludo0777 <peter.wathall@gmail.com>
2012-09-18 20:42:47 +0200

    Added BlockBreakEvent

--- Downloads/BlockListener.java
+++ Downloads/BlockListener.java
@@ -1,4 +1,8 @@
 import org.bukkit.event.Listener;
  
 public class BlockListener implements Listener {
+      @Override
+    public void onBlockBreak (BlockBreakEvent event) {
+        Player p = event.getPlayer();
+    }
     }
