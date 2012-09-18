2f70ba2ca400eecfcc2153f06f0e362096aefa54
ludo0777 <peter.wathall@gmail.com>
2012-09-18 21:16:13 +0200

    Added Enchanting event and cancelled it.

--- Downloads/OtherListener.java
+++ Downloads/OtherListener.java
@@ -1,9 +1,19 @@
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.enchantment.EnchantItemEvent;
+import org.bukkit.entity.Player;
+import org.bukkit.ChatColor;
  
 public class OtherListener implements Listener {
  
       @EventHandler(priority = EventPriority.HIGHEST)
         public void onCreatureSpawn(CreatureSpawnEvent event) {
-         System.out.println("MOB SPAWNED!!!");
+           System.out.println("MOB SPAWNED!!!");
+        }
+      
+      @EventHandler(priority = EventPriority.HIGHEST)
+        public void onEnchant(EnchantItemEvent event) {
+           event.setCancelled(true);
+           event.getPlayer().sendMessage(ChatColor.BLUE + "Enchanting is not allowed!")
+        }
     }
