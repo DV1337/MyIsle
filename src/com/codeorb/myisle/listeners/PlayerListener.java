dea3c4513abc43a9df5cbc6677972d16f736ddfc
Nate Mortensen <nate.richard.mortensen@gmail.com>
2012-09-23 17:46:50 -0600

    Instead of using an array of usernames, we're now reading from the plugin.yml.  Removed Husky from the plugin.yml.  Removed the pointless casts in PlayerListener.

--- src/com/codeorb/myisle/listeners/PlayerListener.java
+++ src/com/codeorb/myisle/listeners/PlayerListener.java
@@ -11,27 +11,21 @@ import org.bukkit.ChatColor;
 import com.codeorb.myisle.MyIsle;
 
 public class PlayerListener implements Listener {
-    String[] devs = new String[]{"evilmidget38","Husky","MYCRAFTisbest", "ludo0777"};
 
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void PlayerJoin(final PlayerJoinEvent event) {
-        Player p = event.getPlayer();
-        boolean isdev = false;
-        for(String dev:devs) {
-            if (p.getName().equals(dev)) {
-                Bukkit.broadcastMessage(ChatColor.BLUE + "[MyIsle] " + ChatColor.GOLD + p.getName() + " made MyIsle");
-                isdev=true;
-            }
-        }
-        if(!isdev) {
-            Bukkit.broadcastMessage(ChatColor.BLUE + "[MyIsle]" + ChatColor.GREEN + p.getName() + "has joined as level <var>");
-            ((Player) p).sendMessage(ChatColor.BLUE + "[MyIsle]" + ChatColor.GREEN + "This is a Isle server");
-        }
-        if (MyIsle.i.voteMsg) return;{
-            ((Player) p).sendMessage(ChatColor.BLUE + "[MyIsle]" + ChatColor.GREEN + "Vote for the server and get a larger isle");
-        }
-    }
-    
-    
-    
+
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void PlayerJoin(final PlayerJoinEvent event) {
+		Player p = event.getPlayer();
+		boolean isdev = false;
+		if (MyIsle.i.getDescription().getAuthors().contains(p.getName()))
+			Bukkit.broadcastMessage(p.getName()+ " is part of the MyIsle dev team!");
+		Bukkit.broadcastMessage(ChatColor.BLUE + "[MyIsle]" + ChatColor.GREEN + p.getName() + "has joined as level <var>");
+		p.sendMessage(ChatColor.BLUE + "[MyIsle]" + ChatColor.GREEN + "This is a Isle server");
+		if (MyIsle.i.voteMsg) return;{
+			p.sendMessage(ChatColor.BLUE + "[MyIsle]" + ChatColor.GREEN + "Vote for the server and get a larger isle");
+		}
+	}
+
+
+
 }
