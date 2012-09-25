2ed379a482cc47e0594519fba3f0750d9d8f2f47
WarmakerT <WarmakerT@gmail.com>
2012-09-26 00:50:05 +0200

    Update src/com/codeorb/myisle/listeners/PlayerListener.java
    Parentheses and spaces after the [MyIsle] tag.

--- src/com/codeorb/myisle/listeners/PlayerListener.java
+++ src/com/codeorb/myisle/listeners/PlayerListener.java
@@ -16,12 +16,14 @@ public class PlayerListener implements Listener {
 	@EventHandler
 	public void PlayerJoin(final PlayerJoinEvent event) {
 		Player p = event.getPlayer();
-		if (MyIsle.i.getDescription().getAuthors().contains(p.getName()))
-			Bukkit.broadcastMessage(p.getName()+ " is part of the MyIsle dev team!");
-		Bukkit.broadcastMessage(ChatColor.BLUE + "[MyIsle]" + ChatColor.GREEN + p.getName() + "has joined as level <var>");
-		p.sendMessage(ChatColor.BLUE + "[MyIsle]" + ChatColor.GREEN + "This is a Isle server");
+		if (MyIsle.i.getDescription().getAuthors().contains(p.getName())) {
+			Bukkit.broadcastMessage(ChatColor.BLUE + p.getName() + ChatColor.GREEN + "§3 is part of the MyIsle development team!");
+		}
+		
+		Bukkit.broadcastMessage(ChatColor.BLUE + "[MyIsle] " + ChatColor.GREEN + p.getName() + " has joined as level <var>");
+		p.sendMessage(ChatColor.BLUE + "[MyIsle] " + ChatColor.GREEN + "This is a Isle server!");
 		if (MyIsle.i.voteMsg) return;{
-			p.sendMessage(ChatColor.BLUE + "[MyIsle]" + ChatColor.GREEN + "Vote for the server and get a larger isle");
+			p.sendMessage(ChatColor.BLUE + "[MyIsle] " + ChatColor.GREEN + "Vote for the server and get a larger isle");
 		}
 	}
 
